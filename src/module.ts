65a81af770d37fe7acd8e9e596bd027f0a40bd32
ashtyn3 <ashtyn@gitmono.com>
2020-12-14 09:20:07 -0500

    made module support block statements

--- src/module.ts
+++ src/module.ts
@@ -1,18 +1,31 @@
-import * as fs from "fs"
+import * as fs from "fs";
 
-export const include = (name:string, parser: Function) => {
-        let file:string = fs.readFileSync(name, "utf-8") 
-
-        let built: string = "";
-        file = file.replace(/#(.*)/g, "");
-        let parsed: Array<string> = []
-        file.split("\n").forEach((l) => {
-            if(!parsed.includes(l)) {
+export const include = (name: string, parser: Function) => {
+    let file: string = fs.readFileSync(name, "utf-8");
+    let built: string = "";
+    file = file.replace(/#(.*)/g, "");
+    let parsed: Array<string> = [];
+    file.split("\n").forEach((l, i) => {
+        if (!parsed.includes(l)) {
+            if (
+                parser(l) == "{" ||
+                l.trim().startsWith("|") ||
+                l.trim().endsWith("block")
+            ) {
+                if (parser(l) == "{") {
+                    built += "{";
+                } else if (!file.split("\n")[i + 1].trim().startsWith("|")) {
+                    built += parser(l.replace("|", "")) + "};";
+                } else {
+                    built += parser(l.replace("|", "")) + "\n";
+                }
+            } else {
                 const line: string = parser(l) + ";";
                 built += line;
-                built = built.replace(/;+/g, ";")
             }
-            parsed.push(l)
-        });
-        return built
-}
+        }
+        parsed.push(l);
+    });
+
+    return built;
+};
